4403c27103b0d89707edc6fbc4a8de0d6c138617
animward <animward@gmail.com>
2024-04-15 12:59:17 -0700

    create components and insert data

---
+++ src/components/MatchList.js
@@ -0,0 +1,14 @@
+import React from "react"; //optional
+import matchData from '../data/matchData';
+import Match from "./Match";
+
+function MatchList(props) {
+  return (
+    <section className="PlayerList MatchList">
+      <h1>Match list</h1>
+      <Match />
+    </section>
+  );
+}
+
+export default MatchList;
---
+++ src/components/PlayerList.js
@@ -0,0 +1,18 @@
+import React from "react"; //optional
+import playerData from '../data/playerData';
+import matchData from '../data/matchData';
+import { preparePlayerData, addWinsToPlayers } from '../helpers/playerHelpers';
+import Player from "./Player";
+
+function PlayerList(props) {
+    const playerDataArray = preparePlayerData(playerData);
+    const parsedPlayerData = addWinsToPlayers(playerDataArray, matchData);
+  return (
+    <section className="PlayerList">
+      <h1>Current participating players</h1>
+      <Player />
+    </section>
+  );
+}
+
+export default PlayerList;

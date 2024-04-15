d9b1fc7d200c437733b6cfc77382e374931b9f1c
animward <animward@gmail.com>
2024-04-15 13:22:22 -0700

    mapping matches and players

--- src/components/MatchList.js
+++ src/components/MatchList.js
@@ -3,14 +3,22 @@ import Match from "./Match";
 
 function MatchList(props) {
 
-    const oneMatch = props.matchData[0];
-
-  return (
-    <section className="PlayerList MatchList">
-      <h1>Match list</h1>
-      <Match players={oneMatch.players} winner={oneMatch.winner} scoreDifference={oneMatch.scoreDifference} />
-    </section>
-  );
-}
+    const matches = props.matchData.map((match) => {
+        return (
+          <Match
+            key={match.matchNumber}
+            players={match.players}
+            winner={match.winner}
+            scoreDifference={match.scoreDifference}
+          />
+        );
+      });
+      
+      return (
+        <section className="PlayerList MatchList">
+          <h1>Match list</h1>
+          {matches}
+        </section>
+)};
 
 export default MatchList;
